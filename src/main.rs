982b9750f3314ef5f505eaf4c39c72fea9815f0e
hms5232 <hms5232@hhming.moe>
2025-09-28 16:36:09 +0800

    feat: get/set battery/power

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod subcommands;
+use clap::{CommandFactory, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: subcommands::item::Command,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    #[allow(unreachable_patterns)]
+    match cli.command {
+        subcommands::item::Command::Power { .. } => {
+            subcommands::item::power::run();
+        }
+        subcommands::item::Command::Battery { limit } => {
+            if let Some(percentage) = limit {
+                if percentage < 1 || percentage > 100 {
+                    eprintln!("Charge limit must be between 1 and 100");
+                    return;
+                }
+                subcommands::item::battery::set_charge_limit(percentage);
+            }
+            subcommands::item::battery::get_charge_limit();
+        }
+        _ => Cli::command().print_help().unwrap(),
+    }
 }
